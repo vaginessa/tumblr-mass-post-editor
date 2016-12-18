9febd23318e6a0422bda612efc26a6c8e6bf8ebc
Bernard Lee <bernardlee82@gmail.com>
2016-12-18 04:21:30 -0500

    Add and set up Passport
    Add and set up Passport.  Rename logger to morgan.

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+
+// Passport
+var passport = require('passport');
+var tumblrStrategy = require('./helpers/tumblr-strategy');
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((object, done) => done(null, object));
+passport.use(tumblrStrategy);
 
 var app = express();
 
@@ -16,7 +24,7 @@ app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
