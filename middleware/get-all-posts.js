3f6bfa922142be171b4bbdb954f3155617a39641
Bernard Lee <bernardlee82@gmail.com>
2016-12-18 17:19:42 -0500

    Add middleware to retrieve all posts
    Add getAllPosts() and getPosts().  getAllPosts() calls getPosts(), which
    recursively calls itself until all posts are retrieved.

---
+++ middleware/get-all-posts.js
@@ -0,0 +1,57 @@
+/* eslint-disable camelcase */
+
+const tumblr = require('tumblr.js');
+const consumer = require('../helpers/consumer');
+let client = null;
+let offset = 0;
+
+function getAllPosts(req, res, next) {
+  client = tumblr.createClient({
+    consumer_key: consumer.key,
+    consumer_secret: consumer.secret,
+    token: req.user.token,
+    token_secret: req.user.tokenSecret
+  });
+
+  getPosts(req.params.blogName).then(data => {
+    res.locals.blog = data;
+
+    return next();
+  }, reason => {
+    console.error('Rejected.');
+    console.error(reason);
+
+    return next(reason);
+  });
+}
+
+function getPosts(blogName, options = { offset }, accumulator = {}) {
+  return new Promise((resolve, reject) => {
+    client.blogPosts(blogName, options, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve(data);
+    });
+  }).then(data => {
+    let allData = null;
+
+    if (!accumulator.posts) {
+      allData = data;
+    } else {
+      allData = accumulator;
+      allData.posts = accumulator.posts.concat(data.posts);
+    }
+
+    if (data.posts.length < 20) {
+      return allData;
+    }
+
+    options.offset += 20;
+
+    return getPosts(blogName, options, allData);
+  });
+}
+
+module.exports = getAllPosts;
