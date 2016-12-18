717aefb7966fe5333fbaf9b52c54719822312298
Bernard Lee <bernardlee82@gmail.com>
2016-12-18 14:18:27 -0500

    Add authorization and callback routes
    Add authorization and callback routes for authenticating with Tumblr
    using OAuth.

---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+/* eslint-disable new-cap */
+
+const router = require('express').Router();
+const passport = require('passport');
+
+// Route: /auth/tumblr
+router.get('/tumblr',
+  passport.authenticate('tumblr'),
+  (_req, res) => {
+    res.redirect('/posts');
+  }
+);
+
+// Route: /auth/tumblr/callback
+router.get('/tumblr/callback',
+  passport.authenticate('tumblr', { failureRedirect: '/login' }),
+  (_req, res) => {
+    res.redirect('/posts');
+  }
+);
+
+module.exports = router;
