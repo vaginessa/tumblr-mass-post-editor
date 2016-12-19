3f2bd6b742f657f8a1d01335b2751a515eef49b3
Bernard Lee <bernardlee82@gmail.com>
2016-12-18 22:43:45 -0500

    Add mass edit post state feature
    Add the option to set all posts to published (public) or private.

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,8 @@
-/* eslint-disable new-cap */
+/* eslint-disable camelcase, new-cap */
 
 const router = require('express').Router();
+const tumblr = require('tumblr.js');
+const consumer = require('../helpers/consumer');
 const ensureAuthenticated = require('../middleware/ensure-authenticated');
 const getAllPosts = require('../middleware/get-all-posts');
 
@@ -13,11 +15,43 @@ router.get('/', (_req, res) => {
 router.get('/:blogName',
   ensureAuthenticated,
   getAllPosts,
-  (_req, res) => {
+  (req, res) => {
     const blog = res.locals.blog.blog;
     const posts = res.locals.blog.posts;
 
+    req.session.blogs[blog.name] = posts.map(post => post.id);
+
     res.render('blog', { blog, posts });
+  }
+);
+
+// Route: /blog/:blogName
+router.get('/:blogName/edit',
+  ensureAuthenticated,
+  (req, res) => {
+    const { blogName } = req.params;
+    const state = req.query.state;
+
+    if (state) {
+      const tumblrClient = tumblr.createClient({
+        consumer_key: consumer.key,
+        consumer_secret: consumer.secret,
+        token: req.user.token,
+        token_secret: req.user.tokenSecret
+      });
+
+      req.session.blogs[blogName].forEach(id => {
+        tumblrClient.editPost(blogName, { id, state }, (err, data) => {
+          if (err) {
+            return err;
+          }
+
+          return data;
+        });
+      });
+    }
+
+    res.redirect(req.headers.referer);
   }
 );
 
