0315d8d2c8ab5c9d3e91887cea891938dab4cbac
Bernard Lee <bernardlee82@gmail.com>
2016-12-18 20:52:54 -0500

    Create a blog session object
    Set req.session.blogs to an empty object on successful authentication.

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ const ensureAuthenticated = require('../middleware/ensure-authenticated');
 router.get('/',
   ensureAuthenticated,
   (req, res, _next) => {
+    if (!req.session.blogs) {
+      req.session.blogs = {};
+    }
+
     const user = req.user.user;
 
     res.render('index', { user });
